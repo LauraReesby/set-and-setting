b8181aaab349302a5aba5f51fc3a92079e525b77
Laura R <lreesb1@gmail.com>
2025-11-05 20:28:46 -0600

    intitialize xcode ios swift project

---
+++ SetAndSettingTests/SetAndSettingTests.swift
@@ -0,0 +1,17 @@
+//
+//  SetAndSettingTests.swift
+//  SetAndSettingTests
+//
+//  Created by Laura Reesby on 11/5/25.
+//
+
+import Testing
+@testable import SetAndSetting
+
+struct SetAndSettingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
